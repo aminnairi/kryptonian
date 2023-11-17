a3dceb3c3b6649a37a13325125faa37a18b2a1c1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-17 11:23:26 +0100

    added more numeric rules

--- src/numeric.ts
+++ src/numeric.ts
@@ -6,3 +6,51 @@ export const between = ({ minimum, maximum, message }: { minimum: number, maximu
     valid: value => value >= minimum && value <= maximum
   };
 };
+
+export const divisibleBy = ({ divisor, message }: { divisor: number, message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value % divisor === 0
+  };
+};
+
+export const notDivisibleBy = ({ divisor, message }: { divisor: number, message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value % divisor !== 0
+  };
+};
+
+export const even = ({ message }: { message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value % 2 === 0
+  };
+};
+export const odd = ({ message }: { message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value % 2 !== 0
+  };
+};
+
+export const positive = ({ message }: { message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value > 0
+  };
+};
+
+export const negative = ({ message }: { message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => value < 0
+  };
+};
+
+export const integer = ({ message }: { message: string }): NumericRule => {
+  return {
+    message,
+    valid: (value: number) => Number.isInteger(value)
+  };
+};
