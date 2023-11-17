c643d10037209d54494357d5edebc112c6e99bc1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-17 11:15:54 +0100

    added minimumLength, maximumLength & lengthBetween functions for lists

--- src/list.ts
+++ src/list.ts
@@ -6,3 +6,24 @@ export const length = ({ length, message }: { length: number, message: string })
     valid: value => value.length === length
   };
 };
+
+export const lengthBetween = ({ minimum, maximum, message }: { minimum: number, maximum: number, message: string }): ListRule => {
+  return {
+    message,
+    valid: value => value.length >= minimum && value.length <= maximum
+  };
+};
+
+export const minimumLength = ({ minimum, message }: { minimum: number, message: string }): ListRule => {
+  return {
+    message,
+    valid: value => value.length >= minimum
+  };
+};
+
+export const maximumLength = ({ maximum, message }: { maximum: number, message: string }): ListRule => {
+  return {
+    message,
+    valid: value => value.length <= maximum
+  };
+};
