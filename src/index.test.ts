0906b746f1e4e96fb164bd7f1027795baa038f86
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-18 13:01:07 +0100

    added tests for jorel

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,55 @@
+import { createServer } from "http";
 import * as Kryptonian from ".";
 
-const protect = Kryptonian.createProtector(Kryptonian.notDefined({
-  message: "This is not undefined"
-}));
+const routes = Kryptonian.Jorel.createRoutes({
+  createUser: {
+    request: Kryptonian.record({
+      message: "This should be a record",
+      rules: [],
+      fields: {
+        email: Kryptonian.text({
+          message: "Email is not a string",
+          rules: []
+        }),
+        password: Kryptonian.text({
+          message: "Password is not a string",
+          rules: []
+        }),
+        confirmation: Kryptonian.text({
+          message: "Confirmation is not a string",
+          rules: []
+        })
+      }
+    }),
+    response: Kryptonian.text({
+      rules: [],
+      message: "Expected a string as the response"
+    })
+  }
+});
 
-const goodData: unknown = undefined;
-const badData: unknown = null;
+const serverRouter = Kryptonian.Jorel.createRouter(routes, {
+  createUser: async () => {
+    return "Successfully created the user";
+  }
+});
 
-const protectionGoneRight = protect(goodData);
-const protectionGoneWrong = protect(badData);
+const server = createServer(serverRouter);
 
-if (protectionGoneRight.success) {
-  console.log(protectionGoneRight.data);
-} else {
-  console.log(protectionGoneRight.errors);
-}
+server.listen(8000, "0.0.0.0", () => {
+  console.log("Kryptonian spaceship launched");
+});
 
-if (protectionGoneWrong.success) {
-  console.log(protectionGoneWrong.data);
-} else {
-  console.log(protectionGoneWrong.errors);
-}
+const clientRouter = Kryptonian.Jorel.getClient(routes);
+
+setTimeout(() => {
+  clientRouter.createUser({
+    confirmation: "",
+    email: "",
+    password: ""
+  }).then(response => {
+    console.log(response);
+  }).catch(() => {
+    console.error("Error")
+  });
+}, 2000);
