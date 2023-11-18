f31bfe3b564cd00822770bf3869ce2d7c4d62061
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-18 19:28:13 +0100

    updated tests for gif

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,117 +1,46 @@
-import { createServer } from "http";
 import * as Kryptonian from ".";
+import * as Http from "http";
 
-const kryptonians: Array<string> = [];
+const kryptonians: Array<string> = [
+  "Jorel",
+  "Kalel",
+  "Zorel"
+];
 
 const routes = Kryptonian.Jorel.createRoutes({
-  createKryptonian: {
-    request: Kryptonian.record({
-      message: "This should be a record",
-      rules: [],
-      fields: {
-        name: Kryptonian.text({
-          message: "Name is not a string",
-          rules: []
-        })
-      }
-    }),
-    response: Kryptonian.text({
-      rules: [],
-      message: "Expected a string as the response"
-    })
-  },
   getKryptonians: {
     request: Kryptonian.none({
-      message: "Expected nothing except null"
+      message: "Request is not null"
     }),
     response: Kryptonian.list({
-      message: "Response is not a list",
+      message: "kryptonians in response is not an array",
       rules: [],
       schema: Kryptonian.text({
-        message: "Response is not a list of text",
+        message: "kryptonians in response is not a list of strings",
         rules: []
       })
     })
   }
 });
 
-// Server code
-
-const serverRouter = Kryptonian.Jorel.createRouter(routes, {
-  createKryptonian: async ({ name }) => {
-    kryptonians.push(name);
-    return "Successfully created the user";
-  },
+const router = Kryptonian.Jorel.createRouter(routes, {
   getKryptonians: async () => {
-    return [
-      ...kryptonians
-    ];
+    return kryptonians;
   }
 });
 
-const serverPort = 8000;
-const serverOrigin = "localhost";
-const serverProtocol = "http";
-const serverUrl = `${serverProtocol}://${serverOrigin}:${serverPort}`
+const server = Http.createServer(router);
 
-const server = createServer(serverRouter);
+server.listen(8000, "0.0.0.0");
 
-server.listen(serverPort, serverOrigin, () => {
-  console.log("Kryptonian spaceship launched");
-});
-
-// Client code
-
-const clientRouter = Kryptonian.Jorel.createClient(serverUrl, routes);
+const client = Kryptonian.Jorel.createClient("http://localhost:8000", routes);
 
 setTimeout(() => {
-  clientRouter.createKryptonian({
-    name: "Kal-el"
-  }).then(() => {
-    console.log("Kal-el saved from krypton explosion");
-  }).catch(() => {
-    console.error("Error")
+  client.getKryptonians(null).then(() => {
+    console.log(kryptonians);
+  }).catch(error => {
+    console.error(error);
+  }).finally(() => {
+    server.close();
   });
 }, 1000);
-
-setTimeout(() => {
-  clientRouter.createKryptonian({
-    name: "Zor-el"
-  }).then(() => {
-    console.log("Zor-el saved from krypton explosion");
-  }).catch(() => {
-    console.error("Error")
-  });
-}, 2000);
-
-setTimeout(() => {
-  clientRouter.createKryptonian({
-    name: "Jor-el"
-  }).then(() => {
-    console.log("Jor-el saved from krypton explosion");
-  }).catch(() => {
-    console.error("Error")
-  });
-}, 3000);
-
-setTimeout(() => {
-  clientRouter.getKryptonians(null).then(kryptonians => {
-    kryptonians.forEach(kryptonian => {
-      console.log(`Found a kryptonian alive: ${kryptonian}`);
-    })
-  }).catch(error => {
-    console.error(String(error));
-  })
-}, 4000);
-
-setTimeout(() => {
-  server.close();
-}, 5000);
-
-// Kryptonian spaceship launched
-// Kal-el saved from krypton explosion
-// Zor-el saved from krypton explosion
-// Jor-el saved from krypton explosion
-// Found a kryptonian alive: Kal-el
-// Found a kryptonian alive: Zor-el
-// Found a kryptonian alive: Jor-el
