9c4df31b9006368787e45d99cebb7d0f6bdeeda5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-19 09:47:48 +0100

    using kryptonian from sources for testing

--- template/client/app.tsx
+++ template/client/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as Kryptonian from "kryptonian";
+import * as Kryptonian from "../../";
 import { routes } from "@template/shared";
 
 const client = Kryptonian.Jorel.createClient({
@@ -11,7 +11,7 @@ export const App = () => {
   const [kryptonians, setKryptonians] = React.useState<Array<string>>([]);
 
   React.useEffect(() => {
-    client.getKryptonians(null).then(kryptonians => {
+    client.getKryptonians().then(kryptonians => {
       setKryptonians(kryptonians);
     }).catch(error => {
       console.error(error);
