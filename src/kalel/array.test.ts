ae36da254dc021e24a72339381bbc792c4d7c759
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-23 17:01:44 +0100

    added tests for arrays

---
+++ src/kalel/array.test.ts
@@ -0,0 +1,57 @@
+import { describe, test, expect } from "vitest";
+import { length, lengthBetween, maximumLength, minimumLength } from "./array";
+
+describe("array", () => {
+  describe("length", () => {
+    test("It should return a rule", () => {
+      const rule = length({
+        length: 2,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid([1, 2])).toEqual(true);
+      expect(rule.valid([2])).toEqual(false);
+    });
+  });
+
+  describe("lengthBetween", () => {
+    test("It should return a rule", () => {
+      const rule = lengthBetween({
+        minimum: 2,
+        maximum: 4,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid([1, 2, 3])).toEqual(true);
+      expect(rule.valid([2, 3, 4, 5, 6])).toEqual(false);
+    });
+  });
+
+  describe("minimumLength", () => {
+    test("It should return a rule", () => {
+      const rule = minimumLength({
+        minimum: 2,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid([1, 2, 3])).toEqual(true);
+      expect(rule.valid([2])).toEqual(false);
+    });
+  });
+
+  describe("maximumLength", () => {
+    test("It should return a rule", () => {
+      const rule = maximumLength({
+        maximum: 2,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid([1, 2])).toEqual(true);
+      expect(rule.valid([2, 3, 4])).toEqual(false);
+    });
+  });
+});
