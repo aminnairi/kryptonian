9c55470f1953df3422167d7bdace8867711a4121
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-23 15:53:53 +0100

    added tests for text

---
+++ src/kalel/text.test.ts
@@ -0,0 +1,42 @@
+import { describe, test, expect } from "vitest";
+import { email, length, minimumLength } from "./text";
+
+describe("text", () => {
+  describe("length", () => {
+    test("It should return a rule", () => {
+      const rule = length({
+        length: 2,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid("")).toEqual(false);
+      expect(rule.valid("ab")).toEqual(true);
+    });
+  });
+
+  describe("minimumLength", () => {
+    test("It should return a rule", () => {
+      const rule = minimumLength({
+        minimum: 2,
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid("")).toEqual(false);
+      expect(rule.valid("ab")).toEqual(true);
+    });
+  });
+
+  describe("email", () => {
+    test("It should return a rule", () => {
+      const rule = email({
+        message: "Message"
+      });
+
+      expect(rule.message).toEqual("Message");
+      expect(rule.valid("email@domain.com")).toEqual(true);
+      expect(rule.valid("email@domain")).toEqual(false);
+    });
+  });
+});
