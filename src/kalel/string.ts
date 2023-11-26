d70f3c5ef2e0024f66362f57b32bf0d7e173184e
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-11-26 15:16:05 +0100

    added a function to check if a string includes another one

--- src/kalel/string.ts
+++ src/kalel/string.ts
@@ -59,6 +59,29 @@ export const email = ({ message}: EmailOptions): Kalel.StringRule => {
     // eslint-disable-next-line no-control-regex
     valid: value => /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/.test(value)
   };
+};
+
+export interface IncludesOptions {
+  /**
+   * The message to attach to the error when the string does not include another
+   */
+  message: string,
+  /**
+   * The string that should be included
+   */
+  string: string
+}
+
+/**
+ * Ensure that a string includes another one
+ */
+export const includes = ({ message, string }: IncludesOptions): Kalel.StringRule => {
+  return {
+    message,
+    valid: value => value.includes(string)
+  };
+};
+
 export interface StartsWithOptions {
   /**
    * The message to attach to the error when the string does not starts with another
