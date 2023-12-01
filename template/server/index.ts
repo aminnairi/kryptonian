e36dad12056ba9aa1e21a8b27460e2fc47e07bd4
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-01 12:35:16 +0100

    using the http adapter instead to serve the app

--- template/server/index.ts
+++ template/server/index.ts
@@ -1,17 +1,19 @@
 import * as Kryptonian from "kryptonian";
-import * as Http from "http";
 import { routes } from "@template/shared";
 import { getKryptonians } from "./routes/getKryptonians";
+import { createHttpServer } from "./adapters/createHttpAdapter";
 
 const router = Kryptonian.Jorel.createRouter({
-  clients: ["http://localhost:5173"],
   routes,
   spaceships: {
     getKryptonians
   }
 });
 
-const server = Http.createServer(router);
+const server = createHttpServer({
+  router,
+  clients: ["http://localhost:5173"]
+});
 
 server.listen(8000, "0.0.0.0", () => {
   console.log("Spaceship launched and ready for communications");
