13dd88f7bf005cfc3592912deaf8edb6b616e10c
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-01 12:30:26 +0100

    added more rollup plugins for handling json and modules

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,9 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import remove from "rollup-plugin-delete";
 import tsconfig from "./tsconfig.json" assert { type: "json" };
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
   input: "src/index.ts",
@@ -11,7 +14,10 @@ export default defineConfig({
       targets: `${tsconfig.compilerOptions.outDir}/*`,
       verbose: true
     }),
+    json(),
     typescript(),
+    commonjs(),
+    nodeResolve(),
     process.env.NODE_ENV === "production" && terser()
   ],
   output: {
