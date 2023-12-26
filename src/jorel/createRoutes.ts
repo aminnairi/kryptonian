c359644ab14f3d6bca10dc9df1af3a26a643b68c
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-26 10:55:43 +0100

    exported the Route & Routes types in their own files

--- src/jorel/createRoutes.ts
+++ src/jorel/createRoutes.ts
@@ -1,25 +1,4 @@
-import * as Kalel from "../kalel";
-
-/**
- * A definition for a route's input (request) and output (response) schema
- */
-export interface Route {
-  /**
-   * The schema that a route should receive
-   */
-  request: Kalel.Schema,
-  /**
-   * The schema that a route must return
-   */
-  response: Kalel.Schema
-}
-
-/**
- * A definition of all routes
- */
-export type Routes = {
-  [key: string]: Route
-}
+import { Routes } from "./Routes";
 
 /**
  * Create a set of routes to be implemented later by the server, and consumed by
