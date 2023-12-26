9aba8a1b7d05c19a8c8cc4b5441aae6694e2aec1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-26 10:54:53 +0100

    exported back the Routes types from the jorel file

--- src/jorel.ts
+++ src/jorel.ts
@@ -1,4 +1,6 @@
 export * from "./jorel/Route";
+export * from "./jorel/Routes";
+export * from "./jorel/createRoute";
 export * from "./jorel/createRoutes";
 export * from "./jorel/createClientRoutes";
 export * from "./jorel/createServerRouter";
